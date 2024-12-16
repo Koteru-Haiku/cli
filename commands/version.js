4a6e006a092d15b3672943960e402f48dcc0d916
ledinhchinh0212 <khongbachofficial@gmail.com>
2024-12-16 23:01:56 +0700

    fixed lost folder in client

--- commands/version.js
+++ commands/version.js
@@ -2,6 +2,11 @@ import fs from 'fs';
 import path from 'path';
 
 export function showVersion() {
-  const packageJson = JSON.parse(fs.readFileSync(path.resolve('package.json'), 'utf8'));
-  console.log(`chx-cli phiên bản: ${packageJson.version}`);
+    try {
+        const packageJson = JSON.parse(fs.readFileSync(path.resolve('package.json'), 'utf8'));
+        console.log(`chx-cli phiên bản: ${packageJson.version}`);
+    }
+    catch(error) {
+        console.log(`chx-cli phiên bản: 1.0.4`);
+    }
 }
